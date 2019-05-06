f59973db339edb4d1ce41a55bc709370208b7d5b
Carl RDJ <carl.rdj@gmail.com>
2019-05-05 20:05:32 -0500

    Primer commit

---
+++ android/app/src/main/java/com/carl/ya_conf_app/MainActivity.java
@@ -0,0 +1,13 @@
+package com.carl.ya_conf_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
